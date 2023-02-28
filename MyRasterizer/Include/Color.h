52587775930d2c4a804f91657bfa1b9f6fc7b873
Lmao-Zedong24 <100137824+Lmao-Zedong24@users.noreply.github.com>
2023-02-27 20:54:39 -0500

    added Color constructor

--- MyRasterizer/Include/Color.h
+++ MyRasterizer/Include/Color.h
@@ -5,6 +5,10 @@ namespace My
 {
 	struct Color
 	{
+		Color() = default;
+		Color(uint8_t p_r, uint8_t p_g, uint8_t p_b, uint8_t p_a) :
+				m_r(p_r), m_g(p_g), m_b(p_b), m_a(p_a) {}
+
 		uint8_t m_r;
 		uint8_t m_g;
 		uint8_t m_b;
